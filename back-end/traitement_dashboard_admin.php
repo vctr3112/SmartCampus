32394981c37bd829df6bd9281beaf2e3b58af497
erika302 <erika.jonsson@edu.ece.fr>
2026-05-31 19:11:11 +0200

    Update traitement_dashboard_admin.php

--- back-end/traitement_dashboard_admin.php
+++ back-end/traitement_dashboard_admin.php
@@ -3,41 +3,36 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "SmartCampus";
-
-//on démarre la session
 session_start();
-
-//on vérifie la session et le rôle
 if (!isset($_SESSION["id_utilisateur"]) || $_SESSION["role"] !== "admin") {
 header("Location: connexion.html");
 exit();
 }
-
-//connexion à la base de données
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 if (!$conn) {
 die("Echec de la connexion: " . mysqli_connect_error());
 }
-
+$id_utilisateur = $_SESSION["id_utilisateur"];
+//on récupère le nom et le prénom de l'admin
+$result_user = mysqli_query($conn, "SELECT nom, prenom FROM Utilisateurs WHERE id_utilisateur = $id_utilisateur");
+$user = mysqli_fetch_assoc($result_user);
 //nombre d'étudiants
 $result_etudiants = mysqli_query($conn, "SELECT COUNT() AS total FROM Etudiant");
 $nb_etudiants = mysqli_fetch_assoc($result_etudiants)["total"];
-
 //nombre d'enseignants
 $result_enseignants = mysqli_query($conn, "SELECT COUNT() AS total FROM Enseignant");
 $nb_enseignants = mysqli_fetch_assoc($result_enseignants)["total"];
-
 //nombre de cours
 $result_cours = mysqli_query($conn, "SELECT COUNT(*) AS total FROM Cours");
 $nb_cours = mysqli_fetch_assoc($result_cours)["total"];
-
+mysqli_close($conn);
 header("Content-Type: application/json");
 echo json_encode([
-    "nb_etudiants" => $nb_etudiants,
-    "nb_enseignants" => $nb_enseignants,
-    "nb_cours" => $nb_cours
+"nom" => $user["nom"],
+"prenom" => $user["prenom"],
+"id_utilisateur" => $id_utilisateur,
+"nb_etudiants" => $nb_etudiants,
+"nb_enseignants" => $nb_enseignants,
+"nb_cours" => $nb_cours
 ]);
-
-mysqli_close($conn);
-?> traitement_dashboard_admin.php
+?>
